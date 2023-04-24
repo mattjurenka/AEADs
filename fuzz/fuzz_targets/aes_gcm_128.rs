c9e8adb3ae4e945bcb67189d3462336aa02f9bef
Matthew Jurenka <matt.jurenka@comcast.net>
2023-04-24 20:28:11 +0000

    initial commit with aes_gcm fuzz

---
+++ fuzz/fuzz_targets/aes_gcm_128.rs
@@ -0,0 +1,17 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use aes_gcm::{
+    aead::{Aead, KeyInit},
+    Aes128Gcm, Nonce
+};
+
+use rand::{rngs::StdRng, SeedableRng};
+
+fuzz_target!(|value: &[u8]| {
+    let key = Aes128Gcm::generate_key(&mut StdRng::seed_from_u64(100u64));
+    let cipher = Aes128Gcm::new(&key);
+    let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
+    let ciphertext = cipher.encrypt(nonce, value.as_ref()).unwrap();
+    let plaintext = cipher.decrypt(nonce, ciphertext.as_ref()).unwrap();
+    assert_eq!(&plaintext, value);
+});
